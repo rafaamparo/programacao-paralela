57624bb7cb4682dc41450cc7cad0fd213987d6ff
João Marinho <jg.limamarinho202@gmail.com>
2025-04-27 21:23:53 -0300

    feat: add input handling for matrix multiplication in MPI program

--- multiplicacao-matrizes-mpi/mpi_matrizes_input.c
+++ multiplicacao-matrizes-mpi/mpi_matrizes_input.c
@@ -13,7 +13,6 @@ int main(int argc, char *argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    tempo_inicial = MPI_Wtime();
     if (m % size != 0 || m < size) {
         if (rank == 0) {
             printf("O número de linhas da matriz A não é divisível pelo número de processos.\n");
@@ -22,12 +21,12 @@ int main(int argc, char *argv[]) {
         return 1;
     }
     linhas_por_proc = m / size;
-
+    
     // essa verificação serve para garantir que o numero de linhas da matriz A (m) pode ser dividido de forma exata entre os processos;
     // se não for divisivel, ou se tivermos mais processos do que linhas, o programa avisa (apenas em p0, para não imprimir mensagens duplicadas) e encerra o programa;
     // isso ocorre porque o Scatter exige que cada processo receba exatamente a mesma quantidade de elementos;
     // se passou pela verificação, entao podemos calcular quantas linhas cada processo vai receber para trabalhar.
-
+    
     particao_a = (int *)malloc(linhas_por_proc * k * sizeof(int));
     // espaço reservado para a parte da matriz A que será enviada a cada processo;
     // cada processo vai receber (linhas_por_proc) linhas da matriz A original;
@@ -38,30 +37,63 @@ int main(int argc, char *argv[]) {
     // espaço reservado para a parte da matriz C que cada processo vai calcular;
     // cada processo vai gerar (linhas_por_proc) linhas de C, cada uma com (n) colunas;
     // a submatriz_c guarda só o resultado parcial, que depois é enviado para p0 para formar a matriz C completa.
-
+    
     if (rank == 0) {
         a = (int *)malloc(m * k * sizeof(int));
         c = (int *)malloc(m * n * sizeof(int));
-        for (int i = 0; i < m * k; i++)
-            a[i] = i + 1;
-        for (int i = 0; i < k; i++) { // matriz B é identidade, para fins de teste
-            for (int j = 0; j < n; j++) {
-                if (i == j)
-                    b[i * n + j] = 1;
-                else
-                    b[i * n + j] = 0;
+        
+        printf("Escreva a matriz A %d×%d (Números separados por espaço):\n", m, k);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < k; j++) {
+                if (scanf("%d", &a[i * k + j]) != 1) {
+                    printf("Erro ao ler a entrada para a matriz A\n");
+                    MPI_Abort(MPI_COMM_WORLD, 1);
+                    return 1;
+                }
             }
         }
+        
+        while (getchar() != '\n');
+        // o buffer do scanf pode ficar com lixo, então limpamos ele para evitar problemas na leitura da matriz B;
+        
+        printf("Escreva a matriz B %d×%d (Números separados por espaço):\n", k, n);
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < n; j++) {
+                if (scanf("%d", &b[i * n + j]) != 1) {
+                    printf("Erro ao ler a entrada para a matriz B\n");
+                    MPI_Abort(MPI_COMM_WORLD, 1);
+                    return 1;
+                }
+            }
+        }
+        printf("\n");
+        printf("Matriz A:\n");
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < k; j++) {
+                printf("%d ", a[i * k + j]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+        printf("Matriz B:\n");
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < n; j++) {
+                printf("%d ", b[i * n + j]);
+            }
+            printf("\n");
+        }
+        printf("\n");
     }
-
+    tempo_inicial = MPI_Wtime();
+    
     // broadcast de B para todos os processos
     MPI_Bcast(b, k * n, MPI_INT, 0, MPI_COMM_WORLD);
-
+    
     // scatter das linhas de A
     MPI_Scatter(a, linhas_por_proc * k, MPI_INT,
-                particao_a, linhas_por_proc * k, MPI_INT,
-                0, MPI_COMM_WORLD);
-    // o processo com o ranque igual a raiz (no caso, definimos a raiz como 0) distribui pelo scatter o conteúdo de A de maneira UNIFORME entre os processos, inclusive o raiz;
+        particao_a, linhas_por_proc * k, MPI_INT,
+        0, MPI_COMM_WORLD);
+        // o processo com o ranque igual a raiz (no caso, definimos a raiz como 0) distribui pelo scatter o conteúdo de A de maneira UNIFORME entre os processos, inclusive o raiz;
     // cada processo recebe uma partição de A com (linhas_por_proc * k) elementos, que é a parte que ele vai calcular, e armazenará no vetor particao_a;
     // passamos esse valor (linhas_por_proc * k) porque o Scatter não consegue enviar uma "linha" da matriz, ele envia elementos;
     // entao se por exemplo, cada processo deve receber 1 linha e a matriz tem 3 colunas, cada processo deve receber 3 elementos.
