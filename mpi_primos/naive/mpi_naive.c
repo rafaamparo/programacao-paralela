cd7a696754804ffb7c5a3cf8a54dd5c26d83005b
João Marinho <jg.limamarinho202@gmail.com>
2025-05-18 11:50:07 -0300

    fix: naive.c aceitando n < 2

--- mpi_primos/naive/mpi_naive.c
+++ mpi_primos/naive/mpi_naive.c
@@ -18,25 +18,53 @@ int main(int argc, char *argv[]) {
 	long int i, n;
 	int meu_ranque, num_procs, inicio, salto;
 
+
+	// argc é a quantidade de argumentos passados na linha de comando
+	// argv é um vetor de strings que contém os argumentos passados
 	if (argc < 2) {
         	printf("Valor inválido! Entre com um valor do maior inteiro\n");
        	 	return 0;
     	} else {
+			// strtol converte uma string em um número inteiro
+			// argv[1] é o número passado como argumento
+
         	n = strtol(argv[1], (char **) NULL, 10);
+
+			// Verifica se o número é menor que 2
+			// Se for, imprime uma mensagem de erro e retorna 0
+			if (n < 2 ) {
+				printf("Valor inválido! Entre com um valor maior que 2\n");
+				return 0;
+			}
        	}
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &meu_ranque);
 	MPI_Comm_size(MPI_COMM_WORLD, &num_procs);	
     t_inicial = MPI_Wtime();
+
     inicio = 3 + meu_ranque*2;
     salto = num_procs*2;
+
+	// Exemplo de como funciona:
+	// Rank 0: Coemça em 3 e incremente de 8 em 8
+	// Rank 1: Começa em 5 e incremente de 8 em 8
+	// Rank 2: Começa em 7 e incremente de 8 em 8
+	// Rank 3: Começa em 9 e incremente de 8 em 8
+
+	// Rank 0: 3, 11, 19, 27, ...
+	// Rank 1: 5, 13, 21, 29, ...
+	// Rank 2: 7, 15, 23, 31, ...
+	// Rank 3: 9, 17, 25, 33, ...
+
+
 	for (i = inicio; i <= n; i += salto) 
 	{	
 		if(primo(i) == 1) cont++;
 	}
 		
 	if(num_procs > 1) {
+		// MPI_Reduce é uma função de redução que combina valores de todos os processos em um único valor 
 		MPI_Reduce(&cont, &total, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 	} else {
 		total = cont;
@@ -46,7 +74,7 @@ int main(int argc, char *argv[]) {
 
 	if (meu_ranque == 0) {
         total += 1;    /* Acrescenta o dois, que também é primo */
-		printf("Quant. de primos entre 1 e n: %d \n", total);
+		printf("Quant. de primos entre 1 e %ld: %d \n", n, total);
 		printf("Tempo de execucao: %0.9f s\n", t_final - t_inicial);	 
 	}
 	MPI_Finalize();
